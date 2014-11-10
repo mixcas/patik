66d15bc3f4dbb7ebdc8995860b2e793ba266bf15
cas <casska@gmail.com>
2014-11-10 10:30:12 -0600

    add - location check

--- client/application.js
+++ client/application.js
@@ -9,24 +9,26 @@ Meteor.startup(function () {
 // Add new drop
 Template.newDrop.events({
   'change .myFileInput': function(event, template) {
-    console.log('uploading');
-    FS.Utility.eachFile(event, function(file) {
-      DropFiles.insert(file, function (err, fileObj) {
-        if(err) throw(err);
-        console.log('success', fileObj);
-        Drops.insert({
-          loc: {
-            type: "Point",
-            coordinates: Session.get('loc') 
-          },
-
-          //coordinates: [ -110.8571443, 32.4586858 ]
-          //coordinates: [ Session.get('loc')[0], Session.get('loc')[1] ]
-          file: fileObj._id,
-          date: new Date()
+    var loc = Session.get('loc');
+    if( 0 != loc[0] && 0 != loc[1] ) {
+      console.log('uploading');
+      FS.Utility.eachFile(event, function(file) {
+        DropFiles.insert(file, function (err, fileObj) {
+          if(err) throw(err);
+          console.log('success', fileObj);
+          Drops.insert({
+            loc: {
+              type: "Point",
+              coordinates: loc
+            },
+            file: fileObj._id,
+            date: new Date()
+          });
         });
       });
-    });
+    } else {
+      alert("Plz, allow us to know your location");
+    }
   }
 });
 
